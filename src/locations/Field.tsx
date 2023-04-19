70cc908aa27eb02ddeb88aa81fda44e1bda04cc1
Gonzalo Medina <gonzalo@bigcartel.com>
2023-04-19 12:40:28 -0700

    log detach

--- src/locations/Field.tsx
+++ src/locations/Field.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TextInput } from '@contentful/f36-components';
 import { FieldExtensionSDK } from '@contentful/app-sdk';
 import { useSDK } from '@contentful/react-apps-toolkit';
@@ -6,43 +6,33 @@ import { useSDK } from '@contentful/react-apps-toolkit';
 const WORDS_PER_MINUTE = 160;
 
 const readingTime = (content: string): number => {
-  let wordCount = 0;
-
-  wordCount = content.split(' ').length;
-  const minutes = Math.ceil(wordCount / WORDS_PER_MINUTE);
-  const response = minutes || 0;
-
-  return response;
+  const wordCount = content.split(' ').length || 0;
+  return Math.ceil(wordCount / WORDS_PER_MINUTE) || 0;
 };
 
 const Field = () => {
   const sdk = useSDK<FieldExtensionSDK>();
   const fieldBody = sdk.entry.fields['body'];
   const fieldBody2 = sdk.entry.fields['body2'];
+  const [timeMessage, setTimeMessage] = useState('');
   const [timeToRead, setTimeToRead] = useState({
     body: 0,
     body2: 0,
   });
-  const [timeMessage, setTimeMessage] = useState('');
 
   useEffect(() => {
     sdk.window.startAutoResizer();
 
     const scanBody = fieldBody.onValueChanged((value: undefined | string) => {
-      if (value) {
-        console.log('body', readingTime(value));
-        setTimeToRead({ ...timeToRead, body: readingTime(value) });
-      }
+      value && setTimeToRead({ ...timeToRead, body: readingTime(value) });
     });
 
     const scanBody2 = fieldBody2.onValueChanged((value: undefined | string) => {
-      if (value) {
-        console.log('body2', readingTime(value));
-        setTimeToRead({ ...timeToRead, body2: readingTime(value) });
-      }
+      value && setTimeToRead({ ...timeToRead, body2: readingTime(value) });
     });
 
     const detach = () => {
+      console.log('detach');
       scanBody();
       scanBody2();
     };
@@ -50,8 +40,6 @@ const Field = () => {
     return () => detach();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [fieldBody, fieldBody2]);
-
-  // , sdk.field, sdk.window
 
   useEffect(() => {
     console.log('timeToRead', timeToRead);
