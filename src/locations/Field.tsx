c89539d320f20d5b4cc032fc4b67a5cb544ab33b
Gonzalo Medina <gonzalo@bigcartel.com>
2023-04-20 11:10:16 -0700

    implement all fields

--- src/locations/Field.tsx
+++ src/locations/Field.tsx
@@ -4,63 +4,71 @@ import { TextInput } from '@contentful/f36-components';
 import { FieldExtensionSDK } from '@contentful/app-sdk';
 import { useSDK } from '@contentful/react-apps-toolkit';
 
-const WORDS_PER_MINUTE = 160;
-
 interface TimeToReadType {
   body: number;
   body2: number;
+  desktopSteps: number;
+  iosSteps: number;
+  androidSteps: number;
 }
+
+const WORDS_PER_MINUTE = 160;
+
+const TIME2READ: TimeToReadType = {
+  body: 0,
+  body2: 0,
+  desktopSteps: 0,
+  iosSteps: 0,
+  androidSteps: 0,
+};
 
 const Field = () => {
   const sdk = useSDK<FieldExtensionSDK>();
   const fieldBody = sdk.entry.fields['body'];
   const fieldBody2 = sdk.entry.fields['body2'];
+  const fieldDesktop = sdk.entry.fields['desktopSteps'];
+  const fieldIOS = sdk.entry.fields['iosSteps'];
+  const fieldAndroid = sdk.entry.fields['androidSteps'];
   const [timeMessage, setTimeMessage] = useState('');
-  // const [timeToRead, setTimeToRead] = useState<TimeToReadType>({
-  //   body: 0,
-  //   body2: 0,
-  // });
-
-  const time2read: TimeToReadType = { body: 0, body2: 0 };
 
   useEffect(() => {
-    // const scanBody = fieldBody.onValueChanged((value: undefined | string) => {
-    //   value && setTimeToRead({ ...timeToRead, body: readingTime(value) });
-    // });
-
-    // const detach = () => {
-    //   console.log('detach');
-    //   scanBody();
-    //   scanBody2();
-    // };
-
-    // return () => detach();
-
     fieldBody.onValueChanged((value: undefined | string) => {
-      console.log('value1 => ', value?.length);
-      console.log('time2read', time2read);
-
-      time2read['body'] = value ? readingTime(value) : 0;
+      TIME2READ['body'] = value ? readingTime(value) : 0;
       updateMessage();
     });
 
     fieldBody2 &&
       fieldBody2.onValueChanged((value: undefined | string) => {
-        console.log('value2 => ', value?.length);
-        console.log('time2read', time2read);
-
-        time2read['body2'] = value ? readingTime(value) : 0;
+        TIME2READ['body2'] = value ? readingTime(value) : 0;
         updateMessage();
       });
-  }, [fieldBody, fieldBody2]);
+
+    fieldDesktop &&
+      fieldDesktop.onValueChanged((value: undefined | string) => {
+        TIME2READ['desktopSteps'] = value ? readingTime(value) : 0;
+        updateMessage();
+      });
+
+    fieldIOS &&
+      fieldIOS.onValueChanged((value: undefined | string) => {
+        TIME2READ['iosSteps'] = value ? readingTime(value) : 0;
+        updateMessage();
+      });
+
+    fieldAndroid &&
+      fieldAndroid.onValueChanged((value: undefined | string) => {
+        TIME2READ['androidSteps'] = value ? readingTime(value) : 0;
+        updateMessage();
+      });
+  }, [fieldBody, fieldBody2, fieldDesktop, fieldIOS, fieldAndroid]);
 
   useEffect(() => sdk.window.startAutoResizer(), []);
 
   const updateMessage = () => {
     let totalTime = 0;
-    console.log('time2read X', time2read);
+    console.log('TIME2READ X', TIME2READ);
 
-    Object.entries(time2read).forEach((value) => {
+    Object.entries(TIME2READ).forEach((value) => {
       console.log('valuessss => ', value[1]);
       totalTime += value[1];
     });
